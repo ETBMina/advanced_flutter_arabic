7a5f4042562e9bad9205106541b4a34191b55b07
Mina Talaat <asu.eng.mina@gmail.com>
2024-02-27 19:09:30 +0200

    Adding login models

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -14,3 +14,28 @@ class SliderViewObject {
 
   SliderViewObject(this.sliderObject, this.numOfSlides, this.currentIndex);
 }
+
+// login models
+class Customer {
+  // Must be not null
+  int id;
+  String name;
+  int numOfNotifications;
+
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contacts {
+  int phone;
+  String email;
+  String link;
+
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(this.customer, this.contacts);
+}
